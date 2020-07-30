e144774a01295cf70a0de182fb1bd3457acaf232
Bastien Nicoud <bastien@Bastiens-MacBook-Pro.local>
2020-07-30 18:44:18 +0200

    Add default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,48 @@
 <template>
-  <div>
-    <Nuxt />
+  <div id="layout-default">
+    <cv-header aria-label="Carbon tutorial">
+      <cv-skip-to-content href="#main-content">
+        Skip to content
+      </cv-skip-to-content>
+
+      <cv-header-name to="/" prefix="Flambeaux">
+        ValTra
+      </cv-header-name>
+
+      <cv-header-nav>
+        <cv-header-menu-item to="up-next">
+          Test onglet
+        </cv-header-menu-item>
+      </cv-header-nav>
+
+      <template slot="header-global">
+        <cv-header-global-action aria-label="Notifications">
+          <notification-20 />
+        </cv-header-global-action>
+        <cv-header-global-action aria-label="User avatar">
+          <user-avatar-20 />
+        </cv-header-global-action>
+        <cv-header-global-action aria-label="App switcher">
+          <app-switcher-20 />
+        </cv-header-global-action>
+      </template>
+    </cv-header>
+    <cv-content id="#main-content">
+      <Nuxt />
+    </cv-content>
   </div>
 </template>
+
+<script>
+import Notification20 from '@carbon/icons-vue/es/notification/20'
+import UserAvatar20 from '@carbon/icons-vue/es/user--avatar/20'
+import AppSwitcher20 from '@carbon/icons-vue/es/app-switcher/20'
+
+export default {
+  components: {
+    Notification20,
+    UserAvatar20,
+    AppSwitcher20
+  }
+}
+</script>
---
+++ pages/up-next.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="container">
+    <h1>A venir</h1>
+    <cv-button kind="secondary">
+      Button
+    </cv-button>
+  </div>
+</template>
