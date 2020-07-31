3b8b0316d4cbc1fa9a45391732fbe4b7285b4e97
Bastien Nicoud <bastien@Bastiens-MacBook-Pro.local>
2020-07-31 20:58:17 +0200

    Add bulma tabs on left

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,47 @@
 <template>
   <div class="container">
-    <h1 class="title">
-      Hello les amis
-    </h1>
+    <div class="columns mt-6">
+      <div class="column is-one-fifth">
+        <aside aside class="menu">
+          <p class="menu-label">
+            General
+          </p>
+          <ul class="menu-list">
+            <li><a>Dashboard</a></li>
+            <li><a>Customers</a></li>
+          </ul>
+          <p class="menu-label">
+            Administration
+          </p>
+          <ul class="menu-list">
+            <li><a>Team Settings</a></li>
+            <li>
+              <a class="is-active">Manage Your Team</a>
+              <ul>
+                <li><a>Members</a></li>
+                <li><a>Plugins</a></li>
+                <li><a>Add a member</a></li>
+              </ul>
+            </li>
+            <li><a>Invitations</a></li>
+            <li><a>Cloud Storage Environment Settings</a></li>
+            <li><a>Authentication</a></li>
+          </ul>
+          <p class="menu-label">
+            Transactions
+          </p>
+          <ul class="menu-list">
+            <li><a>Payments</a></li>
+            <li><a>Transfers</a></li>
+            <li><a>Balance</a></li>
+          </ul>
+        </aside>
+      </div>
+      <div class="column">
+        <h1 class="title">
+          Hello les amis
+        </h1>
+      </div>
+    </div>
   </div>
 </template>
