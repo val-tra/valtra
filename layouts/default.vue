62aba099dcd58deb2e80fe014a89b46e8120880d
Bastien Nicoud <bastien@Bastiens-MacBook-Pro.local>
2020-08-01 11:13:33 +0200

    Added sub-menus for main navigation

--- layouts/default.vue
+++ layouts/default.vue
@@ -32,11 +32,12 @@
               </a>
 
               <div class="navbar-dropdown">
-                <a class="navbar-item">Photos</a>
-                <a class="navbar-item">Nous rejoindre</a>
-                <a class="navbar-item">PV assemblées générales</a>
+                <a class="navbar-item">Qui sommes nous</a>
+                <a class="navbar-item">Organisation du groupe</a>
+                <a class="navbar-item">L'équipe de responsables</a>
                 <hr class="navbar-divider">
-                <a class="navbar-item">Liens externes</a>
+                <a class="navbar-item">Le scoutisme</a>
+                <a class="navbar-item">But du scoutisme</a>
               </div>
             </div>
 
@@ -50,11 +51,17 @@
               </a>
 
               <div class="navbar-dropdown">
-                <a class="navbar-item">Photos</a>
-                <a class="navbar-item">Nous rejoindre</a>
-                <a class="navbar-item">PV assemblées générales</a>
+                <a class="navbar-item">Prochain semestre</a>
                 <hr class="navbar-divider">
-                <a class="navbar-item">Liens externes</a>
+                <a class="navbar-item">Louveteaux (7-11ans)</a>
+                <a class="navbar-item">Eclaireurs (12-16ans)</a>
+                <hr class="navbar-divider">
+                <a class="navbar-item">Week-ends</a>
+                <a class="navbar-item">Camps</a>
+                <a class="navbar-item">Calendrier complet</a>
+                <a class="navbar-item">Lieux et horaires classique</a>
+                <hr class="navbar-divider">
+                <a class="navbar-item">Infos utiles</a>
               </div>
             </div>
 
@@ -78,12 +85,23 @@
           </div>
 
           <div class="navbar-end">
-            <a class="navbar-item">
-              <span class="icon">
-                <i class="fas fa-map-marker-alt" />
-              </span>
-              <span>Contact / Ou</span>
-            </a>
+            <!-- Contact -->
+            <div class="navbar-item has-dropdown is-hoverable">
+              <a class="navbar-link">
+                <span class="icon">
+                  <i class="fas fa-map-marker-alt" />
+                </span>
+                <span>Contact / Ou</span>
+              </a>
+
+              <div class="navbar-dropdown is-right">
+                <a class="navbar-item">Notre Local</a>
+                <a class="navbar-item">Rendez-vous des séances</a>
+                <hr class="navbar-divider">
+                <a class="navbar-item">Contact</a>
+              </div>
+            </div>
+            <!-- LOG IN -->
             <div class="navbar-item">
               <div class="buttons">
                 <a class="button is-light">
