59b1f31f1917ba74a07bd3cdc43604c856277e91
Bastien Nicoud <bastien@Bastiens-MacBook-Pro.local>
2020-08-04 15:57:53 +0200

    Add nuxt-content for dynamic pages

--- pages/index.vue
+++ pages/index.vue
@@ -4,22 +4,31 @@
       <div class="hero-body">
         <div class="container">
           <h1 class="title">
-            Primary bold title
+            Flambeaux du Val-de-travers
           </h1>
           <h2 class="subtitle">
-            Primary bold subtitle
+            Aventure - Amitiés - Nature
           </h2>
         </div>
       </div>
     </section>
     <div class="container">
-      <h1>HOMEPAGE</h1>
+      <div class="content">
+        <nuxt-content :document="page" />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  layout: 'home'
+  layout: 'home',
+  async asyncData ({ $content }) {
+    const page = await $content('home').fetch()
+
+    return {
+      page
+    }
+  }
 }
 </script>
