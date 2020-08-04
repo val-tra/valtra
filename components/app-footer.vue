f63d22b207d4b9c77c435f5668c5dc58aedf35e9
Bastien Nicoud <bastien@Bastiens-MacBook-Pro.local>
2020-08-04 15:46:13 +0200

    Add some footer links

--- components/app-footer.vue
+++ components/app-footer.vue
@@ -5,22 +5,73 @@
         <div class="column is-3">
           <p class="flb-footer-link-title">
             <nuxt-link to="/">
-              Liens sur le site
+              Page d'acceuil
             </nuxt-link>
           </p>
           <p class="flb-footer-link-title">
             <nuxt-link to="/">
-              Liens sur le site
+              Présentation du groupe
             </nuxt-link>
           </p>
           <p class="flb-footer-link">
             <nuxt-link to="/">
-              Liens sur le site
+              L'équipe de responsables
+            </nuxt-link>
+          </p>
+          <p class="flb-footer-link">
+            <nuxt-link to="/">
+              Buts du scoutisme
+            </nuxt-link>
+          </p>
+          <p class="flb-footer-link-title">
+            <nuxt-link to="/">
+              Ou nous trouver
+            </nuxt-link>
+          </p>
+          <p class="flb-footer-link">
+            <nuxt-link to="/">
+              Contact
             </nuxt-link>
           </p>
         </div>
-        <div class="column">
-          Liens externes
+        <div class="column is-3">
+          <p class="flb-footer-link-title">
+            <nuxt-link to="/">
+              Activités du semestre
+            </nuxt-link>
+          </p>
+          <p class="flb-footer-link-title">
+            <nuxt-link to="/">
+              Camps
+            </nuxt-link>
+          </p>
+          <p class="flb-footer-link">
+            <nuxt-link to="/">
+              Lieux et horaires
+            </nuxt-link>
+          </p>
+        </div>
+        <div class="column is-3">
+          <p class="flb-footer-link-title">
+            <nuxt-link to="/">
+              Liens externes
+            </nuxt-link>
+          </p>
+          <p class="flb-footer-link">
+            <nuxt-link to="/">
+              Mouvement scout de suisse
+            </nuxt-link>
+          </p>
+          <p class="flb-footer-link">
+            <nuxt-link to="/">
+              Association du scoutisme neuchatelois
+            </nuxt-link>
+          </p>
+          <p class="flb-footer-link">
+            <nuxt-link to="/">
+              Brigades des flambeaux
+            </nuxt-link>
+          </p>
         </div>
         <div class="column">
           Insta
