66b49a85b28690a7179401a4fd4d865672b06d93
Bastien Nicoud <bastien@Bastiens-MacBook-Pro.local>
2020-08-04 15:08:21 +0200

    Add a specific layout for home page

---
+++ components/app-footer.vue
@@ -0,0 +1,48 @@
+<template>
+  <footer class="footer">
+    <div class="container">
+      <div class="columns">
+        <div class="column">
+          <nuxt-link to="/">
+            Acceuil
+          </nuxt-link>
+        </div>
+        <div class="column">
+          2
+        </div>
+        <div class="column">
+          3
+        </div>
+        <div class="column">
+          4
+        </div>
+      </div>
+    </div>
+    <nav class="level mt-6">
+      <div class="level-item has-text-centered">
+        <div>
+          <p class="heading">Tweets</p>
+          <p class="title">3,456</p>
+        </div>
+      </div>
+      <div class="level-item has-text-centered">
+        <div>
+          <p class="heading">Following</p>
+          <p class="title">123</p>
+        </div>
+      </div>
+      <div class="level-item has-text-centered">
+        <div>
+          <p class="heading">Followers</p>
+          <p class="title">456K</p>
+        </div>
+      </div>
+      <div class="level-item has-text-centered">
+        <div>
+          <p class="heading">Likes</p>
+          <p class="title">789</p>
+        </div>
+      </div>
+    </nav>
+  </footer>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,25 @@
 <template>
-  <div class="container">
-    <h1>HOMEPAGE</h1>
+  <div id="home-page">
+    <section class="hero is-medium is-primary">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">
+            Primary bold title
+          </h1>
+          <h2 class="subtitle">
+            Primary bold subtitle
+          </h2>
+        </div>
+      </div>
+    </section>
+    <div class="container">
+      <h1>HOMEPAGE</h1>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  layout: 'home'
+}
+</script>
