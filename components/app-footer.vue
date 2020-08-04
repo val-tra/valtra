34a765ab9f9809121d22e174f555493e8bab04f0
Bastien Nicoud <bastien@Bastiens-MacBook-Pro.local>
2020-08-04 15:39:32 +0200

    Adding special classes for footer links

--- components/app-footer.vue
+++ components/app-footer.vue
@@ -1,48 +1,31 @@
 <template>
   <footer class="footer">
     <div class="container">
-      <div class="columns">
-        <div class="column">
-          <nuxt-link to="/">
-            Acceuil
-          </nuxt-link>
+      <div class="columns flb-footer-links">
+        <div class="column is-3">
+          <p class="flb-footer-link-title">
+            <nuxt-link to="/">
+              Liens sur le site
+            </nuxt-link>
+          </p>
+          <p class="flb-footer-link-title">
+            <nuxt-link to="/">
+              Liens sur le site
+            </nuxt-link>
+          </p>
+          <p class="flb-footer-link">
+            <nuxt-link to="/">
+              Liens sur le site
+            </nuxt-link>
+          </p>
         </div>
         <div class="column">
-          2
+          Liens externes
         </div>
         <div class="column">
-          3
-        </div>
-        <div class="column">
-          4
+          Insta
         </div>
       </div>
     </div>
-    <nav class="level mt-6">
-      <div class="level-item has-text-centered">
-        <div>
-          <p class="heading">Tweets</p>
-          <p class="title">3,456</p>
-        </div>
-      </div>
-      <div class="level-item has-text-centered">
-        <div>
-          <p class="heading">Following</p>
-          <p class="title">123</p>
-        </div>
-      </div>
-      <div class="level-item has-text-centered">
-        <div>
-          <p class="heading">Followers</p>
-          <p class="title">456K</p>
-        </div>
-      </div>
-      <div class="level-item has-text-centered">
-        <div>
-          <p class="heading">Likes</p>
-          <p class="title">789</p>
-        </div>
-      </div>
-    </nav>
   </footer>
 </template>
